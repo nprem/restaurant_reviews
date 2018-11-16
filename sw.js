9ed08848ad22f164fd9e667637506f3ce5264d46
Preetam Nagoor <PNagoor@slb.com>
2018-11-16 14:33:51 -0600

    initial commit

---
+++ sw.js
@@ -0,0 +1,73 @@
+//******************************************************************************************
+
+// Code referenced and sourced from https://developers.google.com/web/fundamentals/primers/service-workers/
+
+//******************************************************************************************
+
+var cacheName = 'v1';
+
+var cacheFiles = [
+    '/',
+    '/index.html',
+    '/restaurant.html',
+    '/data/restaurants.json',
+    '/css/styles.css',
+    '/js/dbhelper.js',
+    'js/restaurant_info.js',
+    '/js/main.js',
+    '/img/1.jpg',
+    '/img/2.jpg',
+    '/img/3.jpg',
+    '/img/4.jpg',
+    '/img/5.jpg',
+    '/img/6.jpg',
+    '/img/7.jpg',
+    '/img/8.jpg',
+    '/img/9.jpg',
+    '/img/10.jpg', 
+];
+
+self.addEventListener('install', function(event) {
+    event.waitUntil(
+        caches.open(cacheName).then(function(cache) {
+            return cache.addAll(cacheFiles);
+        }).catch(error => {
+            console.log(error);
+        })
+    );   
+});
+
+self.addEventListener('fetch', function(event) {
+    event.respondWith(
+      caches.match(event.request)
+        .then(function(response) {
+          // Cache hit - return response
+          if (response) {
+            return response;
+          }
+  
+          // IMPORTANT: Clone the request. 
+          var fetchRequest = event.request.clone();
+  
+          return fetch(fetchRequest).then(
+            function(response) {
+              // Check if we received a valid response
+              if(!response || response.status !== 200 || response.type !== 'basic') {
+                return response;
+              }
+  
+              // IMPORTANT: Clone the response. 
+              var responseToCache = response.clone();
+  
+              caches.open(CACHE_NAME)
+                .then(function(cache) {
+                  cache.put(event.request, responseToCache);
+                });
+  
+              return response;
+            }
+          );
+        })
+      );
+  });
+
